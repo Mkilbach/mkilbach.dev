ee757fe0ae6fb12f39b5f5dc2b817064d5587689
Mkilbach <mkilbach@gmail.com>
2022-06-16 21:16:30 +0200

    jenkinsfile testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>FTP test</p>
+                <p>FTP test...</p>
             </header>
         </div>
     );
