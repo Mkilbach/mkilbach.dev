d1803e1957b40bf8cb5014fe8f68ca3373a8df11
Mkilbach <mkilbach@gmail.com>
2022-07-11 01:34:11 +0200

    copy email to clipboard with snackbar

--- src/Components/Icons/EmailIcon/index.tsx
+++ src/Components/Icons/EmailIcon/index.tsx
@@ -1,7 +1,8 @@
+import { useCallback, useContext, useEffect, useState } from "react";
 import { Drafts, Mail } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
+import { Alert, IconButton, Snackbar } from "@mui/material";
 import { showMailContext } from "Context/showMailContext";
-import { useContext } from "react";
+import copy from "copy-to-clipboard";
 import SmallIcon from "../SmallIcon";
 
 const EmailIcon = () => {
@@ -11,13 +12,47 @@ const EmailIcon = () => {
         dispatch: setIsOpen,
     } = useContext(showMailContext);
 
+    const [snackBarOpen, setSnackBarOpen] = useState(false);
+
+    const handleIconClick = () => {
+        setIsOpen(x => !x);
+    };
+
+    const copyMailToClipboard = useCallback(() => {
+        if (!isOpen) return;
+
+        copy("mkilbach@gmail.com");
+        setSnackBarOpen(true);
+    }, [isOpen]);
+
+    useEffect(() => {
+        copyMailToClipboard();
+    }, [isOpen, copyMailToClipboard]);
+
+    const handleSnackbarClose = () => setSnackBarOpen(false);
+
     return (
-        <IconButton disabled={blocked} onClick={() => setIsOpen(x => !x)}>
-            <SmallIcon
-                component={isOpen ? Drafts : Mail}
-                color={blocked ? "disabled" : "secondary"}
-            />
-        </IconButton>
+        <>
+            <IconButton disabled={blocked} onClick={handleIconClick}>
+                <SmallIcon
+                    component={isOpen ? Drafts : Mail}
+                    color={blocked ? "disabled" : "secondary"}
+                />
+            </IconButton>
+            <Snackbar
+                open={snackBarOpen}
+                autoHideDuration={6000}
+                onClose={handleSnackbarClose}
+            >
+                <Alert
+                    onClose={handleSnackbarClose}
+                    severity="success"
+                    sx={{ width: "100%" }}
+                >
+                    Email copied to your clipboard!
+                </Alert>
+            </Snackbar>
+        </>
     );
 };
 
