44b128b6c1956313ee4978fb66b2c8dbfeacf9dc
Mkilbach <mkilbach@gmail.com>
2024-07-01 01:19:26 +0200

    snackbar positioning

--- src/Components/Icons/EmailIcon/index.tsx
+++ src/Components/Icons/EmailIcon/index.tsx
@@ -6,54 +6,44 @@ import copy from "copy-to-clipboard";
 import SmallIcon from "../SmallIcon";
 
 const EmailIcon = () => {
-    const {
-        value: isOpen,
-        blocked,
-        dispatch: setIsOpen,
-    } = useContext(showMailContext);
+  const { value: isOpen, blocked, dispatch: setIsOpen } = useContext(showMailContext);
 
-    const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
 
-    const handleIconClick = () => {
-        setIsOpen(x => !x);
-    };
+  const handleIconClick = () => {
+    setIsOpen(x => !x);
+  };
 
-    const copyMailToClipboard = useCallback(() => {
-        if (!isOpen) return;
+  const copyMailToClipboard = useCallback(() => {
+    if (!isOpen) return;
 
-        copy("mkilbach@gmail.com");
-        setSnackBarOpen(true);
-    }, [isOpen]);
+    copy("mkilbach@gmail.com");
+    setSnackBarOpen(true);
+  }, [isOpen]);
 
-    useEffect(() => {
-        copyMailToClipboard();
-    }, [isOpen, copyMailToClipboard]);
+  useEffect(() => {
+    copyMailToClipboard();
+  }, [isOpen, copyMailToClipboard]);
 
-    const handleSnackbarClose = () => setSnackBarOpen(false);
+  const handleSnackbarClose = () => setSnackBarOpen(false);
 
-    return (
-        <>
-            <IconButton disabled={blocked} onClick={handleIconClick}>
-                <SmallIcon
-                    component={isOpen ? Drafts : Mail}
-                    color={blocked ? "disabled" : "secondary"}
-                />
-            </IconButton>
-            <Snackbar
-                open={snackBarOpen}
-                autoHideDuration={6000}
-                onClose={handleSnackbarClose}
-            >
-                <Alert
-                    onClose={handleSnackbarClose}
-                    severity="success"
-                    sx={{ width: "100%" }}
-                >
-                    Email copied to your clipboard!
-                </Alert>
-            </Snackbar>
-        </>
-    );
+  return (
+    <>
+      <IconButton disabled={blocked} onClick={handleIconClick}>
+        <SmallIcon component={isOpen ? Drafts : Mail} color={blocked ? "disabled" : "secondary"} />
+      </IconButton>
+      <Snackbar
+        open={snackBarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+      >
+        <Alert onClose={handleSnackbarClose} severity="success" sx={{ width: "100%" }}>
+          Email copied to your clipboard!
+        </Alert>
+      </Snackbar>
+    </>
+  );
 };
 
 export default EmailIcon;
